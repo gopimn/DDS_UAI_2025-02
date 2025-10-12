ba072f57b6e0040b6b667a16613cd2d52e608e49
jqn1 <154387579+jqn1@users.noreply.github.com>
2025-10-12 12:41:20 -0300

    feat: integrate api with frontend

--- Tarea04/api/app.js
+++ Tarea04/api/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
@@ -12,6 +14,45 @@ app.get("/status", (request,response) => {
     response.send(status);
 });
 
+function reverseInt(num) {
+    let result = num.toString().split("").reverse().join("");
+    return parseInt(result);
+}
+
+function calculateVerifier(rut) {
+    let multiplier = 2; // it starts with 2, then follows with 3,4,5,6,7 and it loops back to 2
+    let result = 0;
+
+    for (let digit of reverseInt(rut).toString()) {
+        digit = parseInt(digit);
+        result += (digit * multiplier);
+
+        if (multiplier == 7) {
+            multiplier = 1;
+        }
+        multiplier++;
+    }
+    result = result - (Math.trunc(result / 11) * 11);
+    result = 11 - result;
+    result = result.toString(); // it needs to be a str since it could be a letter
+
+    if (result == "11"){
+        result = "0";
+    }else if (result == "10") {
+        result = "k";
+    }
+    
+    return result;
+}
+
+app.get("/verify",(request, response) => {
+    const rut = request.query.rut;
+
+    let verifierDigit = calculateVerifier(rut);
+    response.send({
+        "Verifier": verifierDigit
+    })
+})
 // Start the server
 app.listen(PORT, () => {
     console.log(`API listening at http://localhost:${PORT}`);
--- Tarea04/verifyRut.js
+++ Tarea04/verifyRut.js
@@ -1,9 +1,9 @@
 const rutForm = document.getElementById("rutForm");
 
-function reverseInt(num) {
-    let result = num.toString().split("").reverse().join("");
-    return parseInt(result);
-}
+// dont refresh page
+rutForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
 
 
 function rutToNum(rutStr, verifierStr) { 
@@ -24,49 +24,30 @@ function rutToNum(rutStr, verifierStr) {
     return [rut,verifier];
 }
 
-function validateRut(rut, verifier) {
-    verifier = verifier.toLowerCase();
-    let multiplier = 2; // it starts with 2, then follows with 3,4,5,6,7 and it loops back to 2
-    let result = 0;
-
-    for (let digit of reverseInt(rut).toString()) {
-        digit = parseInt(digit);
-        result += (digit * multiplier);
-
-        if (multiplier == 7) {
-            multiplier = 1;
-        }
-        multiplier++;
-    }
-    result = result - (Math.trunc(result / 11) * 11);
-    result = 11 - result;
-    result = result.toString(); // it needs to be a str since it could be a letter
-
-    if (result == "11"){
-        result = 0;
-    }else if (result == "10") {
-        result = "k";
-    }
-    
-    return result;
-}
 
 function validateRutForm(){
     const rutRaw = document.getElementById("rut").value;
-    const verifierDigit = document.getElementById("verifier").value;
+    const verifierDigit = document.getElementById("verifier").value.toLowerCase();
+    const alert = document.getElementById("alert");
+
     let [rut, verifier] = rutToNum(rutRaw, verifierDigit);
 
-    let calculatedVerifier = validateRut(rut,verifier);
-    let isValid = calculatedVerifier == verifier;
+    (async () => {
+        const response = await fetch("http://localhost:3000/verify?rut="+rut);
+        const data = await response.json();
+        let calculatedVerifier = data.Verifier;
 
-    if (isValid){
-        alert("El digito verificador calculado es: "+ calculatedVerifier + "\n¡Rut Valido! :)");
-        return true;
-    }
-    else{
-        alert("El digito verificador calculado es: "+ calculatedVerifier + "\n¡Rut Invalido! :(");
-        return false;
-    } 
+        let isValid = calculatedVerifier == verifier;
+        if (isValid){
+            alert.innerText = ("El digito verificador calculado es: "+ calculatedVerifier + "\n¡Rut Valido! :)");
+            return true;
+        }
+        else{
+            alert.innerText = ("El digito verificador calculado es: "+ calculatedVerifier + "\n¡Rut Invalido! :(");
+            return false;
+        } 
+    })();
+        
 
 }
 
