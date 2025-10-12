b6d0bdcce37ab2d6dbe5782849050e6b610f4722
jqn1 <154387579+jqn1@users.noreply.github.com>
2025-10-12 10:54:45 -0300

    chore: update inputs since the new assignement asks for 2 inputs (rut and verifier digit), i dit that + updated the js functions to support that

---
+++ Tarea04/verifyRut.js
@@ -0,0 +1,72 @@
+const rutForm = document.getElementById("rutForm");
+
+function reverseInt(num) {
+    let result = num.toString().split("").reverse().join("");
+    return parseInt(result);
+}
+
+
+function rutToNum(rutStr, verifierStr) { 
+// return an array with the rut and its verifier digit cleaning all dots and hyphen 
+// we still return the verifier as str because validateRut compares strings for verifier digit
+
+    let cleanRut = "";
+    
+    for (let char of rutStr) {
+        if (char != "." && char != "-") {
+            cleanRut += char;
+        }
+    }
+    
+    let rut = parseInt(cleanRut);
+    let verifier = verifierStr;
+    
+    return [rut,verifier];
+}
+
+function validateRut(rut, verifier) {
+    verifier = verifier.toLowerCase();
+    let multiplier = 2; // it starts with 2, then follows with 3,4,5,6,7 and it loops back to 2
+    let result = 0;
+
+    for (let digit of reverseInt(rut).toString()) {
+        digit = parseInt(digit);
+        result += (digit * multiplier);
+
+        if (multiplier == 7) {
+            multiplier = 1;
+        }
+        multiplier++;
+    }
+    result = result - (Math.trunc(result / 11) * 11);
+    result = 11 - result;
+    result = result.toString(); // it needs to be a str since it could be a letter
+
+    if (result == "11"){
+        result = 0;
+    }else if (result == "10") {
+        result = "k";
+    }
+    
+    if (result == verifier) {
+        return true;
+    } else return false;
+
+}
+
+function validateRutForm(){
+    const rutRaw = document.getElementById("rut").value;
+    const verifierDigit = document.getElementById("verifier").value;
+    let [rut, verifier] = rutToNum(rutRaw, verifierDigit);
+
+    if (validateRut(rut, verifier)){
+        alert("Rut Valido!");
+        return true;
+    }
+    else{
+        alert("Rut Invalido!");
+        return false;
+    } 
+
+}
+
